7d1640b72bb80b5ca6f84e31fe6c3ad5f445018d
karthys <karthys@yahoo-inc.com>
2016-03-12 22:57:47 -0800

    styling

--- fe/app.js
+++ fe/app.js
@@ -7,16 +7,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
-app.use(express.static(__dirname + '/views')); // set the static files location for the static html
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-
-app.use(bodyParser.urlencoded({ extended: false }));  // pull information from html in POST
-app.use(morgan('dev'));                     // log every request to the console
-
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+app.use(bodyParser.urlencoded({ extended: false }));  // pull information from html in POST
+app.use(morgan('dev'));                     // log every request to the console
 
 router.get('/', function(req, res, next) {
     res.render('index', {
@@ -26,11 +25,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/send-money', function(req, res, next) {
-    res.render('send-money.html');
+    res.render('send-money', {
+        page: 'send-money',
+        title: 'Send Money'
+    });
+});
+
+router.post('/send-money', function(req, res, next) {
+    setTimeout(function () {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+        res.write('money-sent');
+        res.end();
+    }, 3000);
 });
 
 router.get('/view-transactions', function(req, res, next) {
-    res.render('view-transactions.html');
+    res.render('view-transactions', {
+        page: 'view-transactions',
+        title: 'Transaction History'
+    });
 });
 
 app.use('/', router);
